d42f653211b7314fdf97f1c3b0122c9b7951c359
Clement HUSSENOT-DESENONGES <clement.hussenot-desenonges@believe.com>
2024-04-10 11:35:11 +0200

    feat: Call the backend

--- plugins/building-blocks/src/components/MainComponent/MainComponent.tsx
+++ plugins/building-blocks/src/components/MainComponent/MainComponent.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { Typography, Grid, Table } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import {
   InfoCard,
   Header,
@@ -11,21 +11,34 @@ import {
 } from '@backstage/core-components';
 import { TableComponent } from '../TableComponent';
 
-export const MainComponent = () => (
-  <Page themeId="tool">
-    <Header title="CI Workflows" subtitle="Optional subtitle">
-      <HeaderLabel label="Owner" value="Team X" />
-      <HeaderLabel label="Lifecycle" value="Alpha" />
-    </Header>
-    <Content>
-      <ContentHeader title="Reusable Building Blocks!">
-        <SupportButton>A description of your plugin goes here.</SupportButton>
-      </ContentHeader>
-      <Grid container spacing={3} direction="column">
-        <Grid item>
-          <TableComponent />
+export const MainComponent = () => { 
+
+  const [buildingBlocks, setBuildingBlocks] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:7007/api/blocks/building-blocks')
+      .then((response) => response.json())
+      .then((data) => setBuildingBlocks(data))
+      .catch((error) => console.error('Error fetching building blocks:', error));
+  }, []);
+
+  return (
+    <Page themeId="tool">
+      <Header title="CI Workflows" subtitle="Optional subtitle">
+        <HeaderLabel label="Owner" value="Team X" />
+        <HeaderLabel label="Lifecycle" value="Alpha" />
+      </Header>
+      <Content>
+        <ContentHeader title="Reusable Building Blocks!">
+          <SupportButton>A description of your plugin goes here.</SupportButton>
+        </ContentHeader>
+        <Typography variant="h1">{buildingBlocks}</Typography>
+        <Grid container spacing={3} direction="column">
+          <Grid item>
+            <TableComponent />
+          </Grid>
         </Grid>
-      </Grid>
-    </Content>
-  </Page>
-);
+      </Content>
+    </Page>
+  );
+};
